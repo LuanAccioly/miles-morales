a66b4736d85539d586694afee6f4bc0d8a6304dd
Luan Accioly <acciolyluan@gmail.com>
2024-05-28 18:35:52 -0300

    feat: extract multiple tables

--- main.py
+++ main.py
@@ -42,14 +42,57 @@ def get_main_spreadsheet():
     open("content/InfoMercado_Dados_Individuais.xlsx", "wb").write(r.content)
     print("Arquivo salvo:\t" + "content/InfoMercado_Dados_Individuais.xlsx")
 
+def extract_csv_files():
+    tabs_info = [
+        {
+            "sheet_name": "007 Lista de Perfis",
+            "table_numeration": "Tabela 001",
+            "footer_row_value": "Topo",
+            "output_name": "perfis.csv"
+        },
+        {
+            "sheet_name": "001 Contratos",
+            "table_numeration": "Tabela 001",
+            "footer_row_value": "Tabela 002",
+            "output_name": "contratos_venda.csv"
+        },
+        {
+            "sheet_name": "001 Contratos",
+            "table_numeration": "Tabela 002",
+            "footer_row_value": "Topo",
+            "output_name": "contratos_compra.csv"
+        },
+        {
+            "sheet_name": "002 Usinas",
+            "table_numeration": "Tabela 001",
+            "footer_row_value": "Topo",
+            "output_name": "usinas.csv"
+        },
+        {
+            "sheet_name": "003 Consumo",
+            "table_numeration": "Tabela 002",
+            "footer_row_value": "Tabela 003",
+            "output_name": "consumo.csv"
+        }
+    ]
+    for tab in tabs_info:
+        proccess_spreadsheet(
+            tab["sheet_name"],
+            tab["table_numeration"],
+            tab["footer_row_value"],
+            tab["output_name"]
+        )
+
+
 
 def proccess_spreadsheet(sheet_name, 
                          header_row_value, 
-                         footer_row_value):
+                         footer_row_value,
+                         output_name):
     
-    print("Extraindo tabela:\t" + sheet_name)
+    print("\n\nExtraindo tabela:\t\t" + sheet_name)
     input_path = "content/InfoMercado_Dados_Individuais.xlsx"
-    output_path = "content/" + sheet_name + ".csv"
+    output_path = "content/" + output_name
     xls = pd.ExcelFile(input_path)
 
     df = pd.read_excel(xls, sheet_name, dtype=str)
@@ -63,9 +106,10 @@ def proccess_spreadsheet(sheet_name,
 
     footer_row = None
     for index, row in df.iterrows():
-        if footer_row_value in row.values:
-            footer_row = index
-            break
+        for value in row.values:
+            if isinstance(value, str) and footer_row_value in value:
+                footer_row = index
+                break
 
     if header_row and footer_row is not None:
         header = df.iloc[header_row]
@@ -77,17 +121,16 @@ def proccess_spreadsheet(sheet_name,
     header = header.dropna(how="all")
     header = header.to_numpy()
 
-    table = table.dropna(how="all")
-    table = table.dropna(axis=1, how="all")
+    # table = table.dropna(how="all")
+    # table = table.dropna(axis=1, how="all")
+
+    table = table.drop(table.columns[0], axis=1)
+    table = table.drop(table.columns[header.size:], axis=1)
 
     table.to_csv(output_path, header=header, index=False)
-    print("Tabela extraída e salva em \t" + output_path)
+    print("Tabela salva em:\t\t" + output_path)
 
 if __name__ == "__main__":
     make_content_dir()
-    get_main_spreadsheet()
-    proccess_spreadsheet(
-        "007 Lista de Perfis",
-        "Tabela 001",
-        "Topo"
-    )
+    # get_main_spreadsheet()
+    extract_csv_files()
