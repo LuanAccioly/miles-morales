09fef77f790973da97a3e7f012b05dcbbc9ad2f6
Luan Accioly <acciolyluan@gmail.com>
2024-05-29 13:15:32 -0300

    feat: add filter CSV files from main spreadsheet

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import os
 
 
 def make_content_dir():
-    path = "content/"
-    if not os.path.exists(path):
+    content_path = "content/"
+    filtered_path = "content/filtered/"
+    if not os.path.exists(content_path):
         print("Criando diretório 'content'")
-        os.makedirs(path)
+        os.makedirs(content_path)
+        os.makedirs(filtered_path)
     else:
         print("Diretório content/ já existe\n\n")
 
@@ -42,54 +44,51 @@ def get_main_spreadsheet():
     open("content/InfoMercado_Dados_Individuais.xlsx", "wb").write(r.content)
     print("Arquivo salvo:\t" + "content/InfoMercado_Dados_Individuais.xlsx")
 
+
 def extract_csv_files():
     tabs_info = [
         {
             "sheet_name": "007 Lista de Perfis",
             "table_numeration": "Tabela 001",
             "footer_row_value": "Topo",
-            "output_name": "perfis.csv"
+            "output_name": "perfis.csv",
         },
         {
             "sheet_name": "001 Contratos",
             "table_numeration": "Tabela 001",
             "footer_row_value": "Tabela 002",
-            "output_name": "contratos_venda.csv"
+            "output_name": "contratos_venda.csv",
         },
         {
             "sheet_name": "001 Contratos",
             "table_numeration": "Tabela 002",
             "footer_row_value": "Topo",
-            "output_name": "contratos_compra.csv"
+            "output_name": "contratos_compra.csv",
         },
         {
             "sheet_name": "002 Usinas",
             "table_numeration": "Tabela 001",
             "footer_row_value": "Topo",
-            "output_name": "usinas.csv"
+            "output_name": "usinas.csv",
         },
         {
             "sheet_name": "003 Consumo",
             "table_numeration": "Tabela 002",
             "footer_row_value": "Tabela 003",
-            "output_name": "consumo.csv"
-        }
+            "output_name": "consumo.csv",
+        },
     ]
     for tab in tabs_info:
         proccess_spreadsheet(
             tab["sheet_name"],
             tab["table_numeration"],
             tab["footer_row_value"],
-            tab["output_name"]
+            tab["output_name"],
         )
 
 
+def proccess_spreadsheet(sheet_name, header_row_value, footer_row_value, output_name):
 
-def proccess_spreadsheet(sheet_name, 
-                         header_row_value, 
-                         footer_row_value,
-                         output_name):
-    
     print("\n\nExtraindo tabela:\t\t" + sheet_name)
     input_path = "content/InfoMercado_Dados_Individuais.xlsx"
     output_path = "content/" + output_name
@@ -125,12 +124,51 @@ def proccess_spreadsheet(sheet_name,
     # table = table.dropna(axis=1, how="all")
 
     table = table.drop(table.columns[0], axis=1)
-    table = table.drop(table.columns[header.size:], axis=1)
+    table = table.drop(table.columns[header.size :], axis=1)
 
     table.to_csv(output_path, header=header, index=False)
     print("Tabela salva em:\t\t" + output_path)
 
+
+def compare_with_profiles(csv_file):
+    csv_name = csv_file.split("/")[-1]
+    profiles_df = pd.read_csv("content/perfis.csv")
+    valid_cod_profile = profiles_df["Cód. Perfil de Agente"]
+
+    csv_df = pd.read_csv(csv_file)
+    filtered_csv = csv_df[csv_df["Cód. Perfil"].isin(valid_cod_profile)]
+
+    filtered_csv = filtered_csv.dropna(subset=["Cód. Perfil"])
+
+    # Exibição dos perfis removidos ==========
+    csv_df = csv_df.dropna(subset=["Cód. Perfil"])
+    profile_id = csv_df["Cód. Perfil"]
+
+    print("====================================")
+    print("\n\nPerfis removidos em: " + csv_file)
+    for id in profile_id:
+        if id not in valid_cod_profile.values:
+            print(int(id), end=" ")
+    print("\n====================================\n\n")
+    #  =======================================
+
+    filtered_csv.to_csv(f"content/filtered/{csv_name}_filtrado.csv", index=False)
+
+
+def filter_csv_files():
+    csv_files = [
+        "content/contratos_venda.csv",
+        "content/contratos_compra.csv",
+        "content/usinas.csv",
+        "content/consumo.csv",
+    ]
+
+    for csv_file in csv_files:
+        compare_with_profiles(csv_file)
+
+
 if __name__ == "__main__":
-    make_content_dir()
+    # make_content_dir()
     # get_main_spreadsheet()
     extract_csv_files()
+    filter_csv_files()
